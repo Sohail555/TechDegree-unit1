3c1379058b04bc34a7ce022b3bb70635ba963ead
Sohail555 <43937016+Sohail555@users.noreply.github.com>
2018-10-15 22:06:17 +0100

    update1

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,14 +1,16 @@
 import random
 
 def start_game():
-   random_number = int(random.randint(1, 20))
+   random_number = int(random.randint(1, 10))
    print("Welcome to the Guessing Game!")
    name = input(" Can I take your name challenger ")
    print(name, "You have to guess the correct number in the fewest attempts")
    print(name, "Are you ready!")
 
 
+def _game()
 guess = 0
+
 attempt_count = 1
 while guess != randon_number:
     guess = int(input("New number: "))

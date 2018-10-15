696ce5935ade96fc1179326b47d1fa7664a60140
Sohail555 <43937016+Sohail555@users.noreply.github.com>
2018-10-15 18:21:52 +0100

    Initial commit

---
+++ Guessing Game.py
@@ -0,0 +1,24 @@
+import random
+
+def start_game():
+   random_number = int(random.randint(1, 20))
+   print("Welcome to the Guessing Game!")
+   name = input(" Can I take your name challenger ")
+   print(name, "You have to guess the correct number in the fewest attempts")
+   print(name, "Are you ready!")
+
+
+guess = 0
+attempt_count = 1
+while guess != randon_number:
+    guess = int(input("New number: "))
+    if random_number > guess:
+        print("The number is lower")
+        continue
+    attempt_count += 1
+    elif random_number < guess:
+        print("The number is higher")
+        continue
+    attempt_count += 1
+
+print("You are correct")
